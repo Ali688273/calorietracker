f8fee4f6619ab813af61c8df2b7eb1c0dae8728a
Ali688273 <iiii688273@gmail.com>
2026-09-06 17:40:12 +0330

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,54 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+}
+
+android {
+    namespace = "com.amarmasraf.calorietracker"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.amarmasraf.calorietracker"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0.0"
+    }
+
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.keystore")
+            storePassword = "CaloriePassword123"
+            keyAlias = "caloriekey"
+            keyPassword = "CaloriePassword123"
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("release")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+}
